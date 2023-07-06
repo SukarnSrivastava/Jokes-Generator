0a4dc6d83033f16afdc0352c5170e3a7d2c9a92e
Sukarn Srivastava <sukarnsrivastava@gmail.com>
2023-07-06 23:55:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,188 @@
+let humourJokes = [
+    "How do you make a tissue dance? Put a little boogie in it.",
+    "What is the tallest building in the entire world?  The library, because it has so many stories.",
+    "How do trees access the internet? They log in.",
+    "What do you get when you cross a fish and an elephant?  Swimming trunks.",
+    "Why did the painting go to jail?  It was framed.",
+    "Why did Cinderella get kicked off the soccer team? Because she ran away from the ball.",
+    "Why do we tell actors to break a leg?  Because every play has a cast.",
+    "Helvetica and Times New Roman walk into a bar. The bartender says, “We don’t serve your type.",
+    "Why wouldn’t the shrimp share his treasure? Because he was a little shellfish.",
+    "Why should you knock on the refrigerator before opening the door? In case there is a salad dressing",
+    "When is a door not a door? When it is ajar.",
+    "What did the fish say when he swam into a wall? Dam.",
+    "Why did the scarecrow win an award? Because he was outstanding in his field.",
+    "Why did the melon jump into the lake? It wanted to be a water-melon.",
+    "What did the duck say when it bought lipstick? 'Put it on my bill.'",
+    "What do you call a pig that does karate? A pork chop.",
+    "What has a bed that you can’t sleep in? A river.",
+    "Why were the teacher’s eyes crossed? She couldn’t control her pupils.",
+    "What starts with E, ends with E, and has only 1 letter in it? An Envelope.",
+    "How does the ocean say hello? It waves.",
+    "What lights up a soccer stadium? A soccer match.",
+    "What creature is smarter than a talking parrot? A spelling bee.",
+    "Which U.S. state has the smallest soft drinks? Minnesota (as in, “mini-soda”).",
+    "Why couldn’t the leopard play hide and seek? Because he was always spotted.",
+    "Apparently, you can’t use “beef stew” as a password. It’s not stroganoff.",
+    "Why did the drum take a nap? It was beat.",
+    "Where do hamburgers go dancing?  They go to the meat-ball.",
+    "Why did the tomato turn red?  It saw the salad dressing.",
+    "Why shouldn’t you write with a broken pencil? Because it’s pointless.",
+    "What do you call two monkeys that share an Amazon account? Prime mates.",
+    "Why are teddy bears never hungry? Because they’re always stuffed!",
+    "Why did the tree go to the dentist? It needed a root canal.",
+    "If athletes get athlete’s foot, what do elves get? Mistle-toes.",
+    "Why couldn’t the pony sing? Because she was a little hoarse.",
+    "Where do cows go for entertainment? The mooooo-vies!",
+    "What do you call a pig that does karate? A pork chop.",
+    "How does NASA organize a party? They planet.",
+    "Why did Adele cross the road?  To sing, “Hello from the other side!",
+    "What runs around a yard without actually moving? A fence.",
+    "What’s an astronaut’s favorite candy? A Mars bar.",
+    "Where do sheep go to get their hair cut?  The baa-baa shop.",
+    "Why are there gates around cemeteries? Because people are dying to get in.",
+    "What do you get when you cross a snake with a pie? A pie-thon!",
+    "Why is Peter Pan always flying? He Neverlands.",
+    "What’s the most musical part of the chicken? The drumstick."
+  ];
+  
+  let programmingJokes = ["Why does C get all the girls, and Java gets none? Because C doesn’t treat them as objects.",
+    "Two programmers are talking about their social life, and one says:The only date I get is the Java Update.",
+    "Two students, a boy and a girl, are sitting in a class when the boy tries to look under the girl’s shirt.Girl: Hey! What are you doing? Boy: Members from the same class can access private area!",
+    "A guy is chain-smoking outside when an old lady sees him and says:You shouldn’t smoke, those things will kill you. Look at the warning on the box!The guy continues puffing and says: I don’t care. I am a Java Programmer. We don’t care about warnings, only about errors.",
+    "Why do Java developers wear glasses?Because they don’t C#!",
+    "I had a problem. I used Java. Now, I have a ProblemFactory.",
+    "Knock, knock. Who’s there.[very long pause…] Java!",
+    "Java and C are telling jokes to each other. C writes something on the blackboard, and asks Java: Do you get the reference? Java didn’t.",
+    " A C++ walks into a bar and sees a C. C is drunk, falling on the floor, spitting and swearing. How classless! – says C++.",
+    "What is an object-oriented way to become wealthy? Inheritance.",
+    "C programmers never die. They are just <cast> into VOID.",
+    "What is a programmer?  An organism that turns caffeine and fast food into software.",
+    "A guy meets a girl who wants to be a web developer. Falls in love instantly. He comes to her place to see a house full of pet spiders.",
+    "An optimist says: “The glass is half-full” A pessimist says: “The glass is half empty”. A programmer says: “The glass is twice as large as necessary!”",
+    "A programmer talks with a philanthropist: If you want to make the world a better place, why don’t you get the original source code?",
+    "Why did the programmer quit his job? He never got arrays.",
+    "Programmers like to solve problems. When they run out of problems. They create new problems!",
+    " A programmer buys a kilo of bananas in the market. He returns angry after a while and says: There are 24 grams off!",
+    "Programming is like sex. One mistake and you have to support it for the rest of your life.",
+    "A programmer ends up in hell. What did I do? I was a kind and honest person! You disabled right-clicks on your web-pages – the devil responds.",
+    "A programmer sees “While there’s hope, there’s life” written on the wall. He decides to edit it and writes: “While there’s code, there’s bug”.",
+    "What is an algorithm? A word programmers use when they don’t want to explain what they did.",
+    "How does machine learning work? Q: What is 11 times 11? A: It’s 65. Q: Not at all. It’s 121. A: It’s 121.",
+    "What is hardware? A part of your computer that you can kick.",
+    "Two bytes meet, and one says: Wow, you don’t look well. Are you ill? The other responds: Nah, I’m just feeling a bit off.",
+    "A computer is mightier than the pen, sword, and usually the programmer.",
+    "A SQL query walks into a bar, walks up to two tables, and asks – Can I join you?",
+    "Why are computer like men? –To get them going, you first have to turn them on. –They have a lot of data, but are still clueless. –They should help you solve a problem, but usually, they are the cause of the problem. –Once you decide to commit to one, you realize you could’ve gotten a better model if you waited a bit.",
+    "Java, Phyton, C++, and ANSI hold a meeting. Java: OK, people. How to make women more interested in us? C++: Maybe more exceptions? Phyton: We should define our methods? ANSI-C: Maybe stop treating them as objects?",
+    "You are the ; to my statements",
+    "Why did the programmer quit her job? Because she didn’t get arrays",
+    "What did the Java Code say to the C code? You’ve got no class",
+    "A programmer puts two glasses on his bedside table before going to sleep. A full one, in case he gets thirsty, and an empty one, in case he doesn’t",
+    "Why do programmers prefer dark mode? Because light attracts bugs.",
+  ]
+  
+  let adultJokes = [
+    "There’s a fine line between a numerator and a denominator. (…Only a fraction of people will get this clean joke.)",
+    "What did the toaster say to the slice of bread? “I want you inside me.”",
+    "What do dentists call their x-rays?Tooth pics!",
+    "I can never take my dog to the park because the ducks keep trying to bite him. I guess that’s what I get for buying a pure bread dog.",
+    "Did you hear about the cheese factory that exploded in France? There was nothing left but de Brie.",
+    "Want to hear a joke about a roof? The first one’s on the house.",
+    "Why doesn’t Dracula have any friends? Well, honestly, he’s a real pain in the neck.",
+    "My teachers told me I’d never amount to much since I procrastinate so much. I told them, “Just wait!”",
+    "How much do teddy bears never want to eat anything? Because they’re always stuffed.",
+    "You know, it was so cold in D.C. the other day, I saw a politician with his hands in his own pockets.",
+    "How many tickles does it take to get an octopus to laugh? Ten tickles",
+    "Why are toilets always so good at poker? They always get a flush",
+    "What gets wetter the more it dries? A towel.",
+    "Where does a waitress with only one leg work? IHOP.",
+    "What does a house wear? Address!",
+    "Why is Peter Pan always flying? Because he Neverlands. (I love this joke because it never grows old.)",
+    "Why aren’t koalas considered bears? They don’t have the right koala-fications.",
+    "How do you measure a snake? In inches—they don’t have feet.",
+    "Did you hear about the first restaurant to open on the moon? It had great food, but no atmosphere.",
+    "I entered ten puns in a contest to see which would win.No pun in ten did.",
+    "Why were they called the “dark ages?”Because there were a lot of knights.",
+    "How does NASA organize a party? They planet.",
+    "Two windmills are standing on a wind farm. One asks, ‘What’s your favorite kind of music?’The other replies, ,‘I’m a big metal fan.’",
+    "What do you get from a pampered cow? Spoiled milk.",
+    "I took the shell off of my racing snail, thinking it would make him faster. But if anything, it made him more sluggish.",
+    "You heard the rumor going around about butter? Never mind, I shouldn’t spread it.",
+    "What’s the man thing about Switzerland?I don’t know, but the flag is a big plus.",
+    "The first rule of the Alzheimer’s club is…Wait, where are we again?",
+    "Where should you go in the room if you’re feeling cold? The corner—they’re usually 90 degrees.",
+    "When does a joke become a ‘dad’ joke? When it becomes apparent.",
+    "Why did the bullet end up losing his job? He got fired.",
+    "What does a nosey pepper do? It gets jalapeño business.",
+    "What kind of shorts do clouds wear? Thunderpants",
+    "Why should you never trust stairs? They’re always up to something.",
+    "Why do ducks have feathers? To cover their butt quacks!",
+    "What’s the difference between a hippo and a zippo? One is really heavy and the other’s a little lighter.",
+    "Did you hear about the fire at the circus? It was in tents!",
+    "Do you want to hear a construction joke? Sorry, I’m still working on it.",
+    "What did one ocean say to the other ocean? Nothing, it just waved.",
+    "A cute elderly couple, both in their 80s, paid a visit to a s*x therapist. The therapist warmly greeted them, saying, “You two make such a lovely couple. How can I assist you?”",
+    "The man responded, “Do you mind watching us have intercourse?” After a brief moment of contemplation, the therapist agreed, seeing no harm in the matter.",
+    "Once the couple finished their intimate session, the therapist gave them her evaluation, saying, “Your lovemaking was perfectly normal. No issues with either of your performances.” With a friendly smile, she charged them her regular $90 and wished them a pleasant day.",
+    "Surprisingly, the couple returned the next week, repeating the same routine. In fact, this continued every Wednesday for six weeks straight. Each time, they would arrive, engage in their intimate act in front of the therapist, pay the fee, and leave.",
+    "After the sixth visit, the therapist couldn’t contain her curiosity any longer. She asked, “Can I ask why you guys keep making appointments to see me? Especially since I never find anything to improve in your lovemaking habits.”",
+    "Without missing a beat, the old man started to explain, “Well, you see, we can’t do it at my place because my wife is there, and we can’t do it at her place because her husband is there. And even the cheapest hotels charge $130 a night. But coming here only costs me $90.”",
+  ]
+  
+   let storingCat = "";
+  // let categories = document.getElementsByClassName("categories")[0];
+  // categories.addEventListener("click", e => {
+  //   randomJokesGenerator(e.target.id);
+  //   storingCat = e.target.id;
+  //   catButton(e.target.id);
+  // });
+  
+  let categories = document.getElementsByClassName("categories")[0];
+  let selectedButton = null; // Track the previously selected button
+  
+  categories.addEventListener("click", e => {
+    if (selectedButton) {
+      selectedButton.style.backgroundColor = ""; // Unset the color of the previously selected button
+      selectedButton.style.color = "";
+    }
+  
+    randomJokesGenerator(e.target.id);
+    storingCat = e.target.id;
+    catButton(e.target); // Pass the entire target element to the catButton function
+  });
+  
+  function catButton(button) {
+    button.style.backgroundColor = "#7a80f5"; // Set the color of the clicked button to red
+    button.style.color = "white";
+    selectedButton = button; // Update the selectedButton variable to track the current selection
+  }
+  
+  
+  
+  function randomJokesGenerator(jokes) {
+    let jokesData;
+    if (jokes === "pjokes") {
+      jokesData = programmingJokes;
+    }
+    else if (jokes === "ajokes") {
+      jokesData = adultJokes;
+    }
+    else if (jokes === "hjokes") {
+      jokesData = humourJokes;
+    }
+    else {
+      let randomNum = Math.floor(Math.random() * 2);
+      let cat = [adultJokes, programmingJokes, humourJokes];
+      jokesData = cat[randomNum];
+    }
+  
+    let randomNum = Math.floor(Math.random() * (jokesData.length - 1));
+    let randomJokes = jokesData[randomNum];
+    document.getElementById("joke").innerHTML = randomJokes;
+  }
+  
+  let a = document.getElementById("btn");
+  a.addEventListener(("click"), e => randomJokesGenerator(storingCat));
+  
+  
